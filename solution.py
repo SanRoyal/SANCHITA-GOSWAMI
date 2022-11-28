987cf6eb9a26721af736f5c75fbd9b9dd06429de
SanRoyal <117883424+SanRoyal@users.noreply.github.com>
2022-11-29 00:34:17 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,47 @@
+import cv2
+from tracker import *
+
+# Create tracker object
+tracker = EuclideanDistTracker()
+
+cap = cv2.VideoCapture("Hackathon video.mov")
+
+# Object detection from Stable camera
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+while True:
+    ret, frame = cap.read()
+    height, width, _ = frame.shape
+    frame=cv2.resize(frame,(1200,700))
+    # Extract Region of interest
+    roi = frame[0: 700,0: 1200]
+    # roi = frame
+    # 1. Object Detection
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+    for cnt in contours:
+        # Calculate area
+        area = cv2.contourArea(cnt)
+        if area > 10:
+            x, y, w, h = cv2.boundingRect(cnt)
+            detections.append([x, y, w, h])
+
+    # 2. Object Tracking
+    boxes_ids = tracker.update(detections)
+    for box_id in boxes_ids:
+        x, y, w, h, id = box_id
+        cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow("roi", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
